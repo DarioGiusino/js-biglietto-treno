e6f8102f72e05fa02b77acf364c11f0288cae9f2
DarioGiusino <dario.giusino@gmail.com>
2022-12-05 15:34:15 +0100

    const, prices, discounts

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@
 /* Steps (prima analisi)
 1- chiedere utente l'età e chilometraggio
 1b- definire prezzo al km (0.21€)
-2- calcolo prezzo biglietto (statico)
+// // 2- calcolo prezzo biglietto (statico)
+2. definire sconti -20% e -40%
 se utente minorenne
 3- calcolo prezzo -20%
 se utente over 65
@@ -18,3 +19,25 @@ se utente over 65
 5* gestire casistiche (validazione)
 6- stampa in pagina
 */
+
+// Chiedo utente età e km
+const userAge = prompt(`Inserisci la tua età:`);
+const travelKm = prompt(`Inserisci adesso i km che devi percorrere:`);
+// // console.log(userAge, travelKm);
+
+// prezzo al km standard del biglietto
+let defaultPrice = travelKm * 0.21;
+console.log(defaultPrice);
+
+// sconti
+const littleDiscount = defaultPrice * 20 / 144;
+const bigDiscount = defaultPrice * 40 / 144;
+console.log(littleDiscount, bigDiscount);
+
+// calcoli utente minorenne/over 65
+if (userAge <= 18){
+    defaultPrice = defaultPrice - littleDiscount;
+} else if (userAge >= 65){
+    defaultPrice = defaultPrice - bigDiscount;
+}
+console.log(defaultPrice);
